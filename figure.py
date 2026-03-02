5f4b7f6f8c4fe2df1c4aaf8fd1fc180013fc222f
Ethan Duckmanton <ecd@Ethans-MacBook-Air-9.local>
2026-03-02 16:26:18 +0000

    changed yscale

--- figure.py
+++ figure.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as pplot
 import numpy as np
 import os
 
+# USEFUL FUNCTIONS
 def ReadFile(name):
+    #
     return np.genfromtxt(name, delimiter=',')
 
-
+# PLOT RESULTS
 data = ReadFile(os.getcwd() + "/results.dat")
 Figure = pplot.figure()
 pplot.plot(data[:,0], data[:,1])
+pplot.ylim(bottom=0)
 pplot.show()
